d678c12333e928a77d160692696140a89c51bef1
urangunang <145663184+urangunang@users.noreply.github.com>
2023-11-27 22:58:42 +0700

    Update config.py
    Signed-off-by: urangunang <145663184+urangunang@users.noreply.github.com>

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 150))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+OWNER_ID = int(getenv("OWNER_ID", 5868007296))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
